368a246a4e9882bef408385f4e5cc7b67feb9779
Roshan Khadka <rkhadka@hamropatro.com>
2023-04-22 10:11:02 +0545

    implemented stock detail page

--- src/component/StockDetail.js
+++ src/component/StockDetail.js
@@ -4,7 +4,23 @@ import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
-import { DialogContentText } from "@material-ui/core";
+import {
+  DialogContentText,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
 
 export function StockDetail(props) {
   const { open, handleClose, detail } = props;
@@ -19,8 +35,42 @@ export function StockDetail(props) {
       <DialogTitle id="alert-dialog-title">Stock Detail</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-          {console.log("")}
-          {JSON.stringify(detail)}
+          {/* {console.log("")}
+          {JSON.stringify(detail)} */}
+
+          {/* <List component="nav" aria-label="company data">
+            {Object.entries(detail).map(([key, value]) => (
+              <ListItem key={key}>
+                <ListItemText primary={key} secondary={value} />
+              </ListItem>
+            ))}
+          </List> */}
+
+          {/* {Object.entries(detail).map(([key, value]) => (
+            <div key={key}>
+              <span>{key} : </span>
+              <span>{value}</span>
+            </div>
+          ))} */}
+
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell className="table-header">Key</TableCell>
+                  <TableCell className="table-header">Value</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Object.entries(detail).map(([key, value]) => (
+                  <TableRow key={key}>
+                    <TableCell>{key}</TableCell>
+                    <TableCell>{value}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </DialogContentText>
       </DialogContent>
       <DialogActions>
